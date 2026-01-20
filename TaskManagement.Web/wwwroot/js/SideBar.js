7b5509572e8f1dcb03b629f83a7937ecc5485d8e
Zakaullah898 <zakaullahlaar9@gmail.com>
2026-01-20 21:36:09 +0500

    Adding separate javascript files for sidebar and navbar  and add partial  view  files for both files  .  Sidebar  and Navbar and add functionality of opening and close of side bar when cursor come over the side bar using js. And setup dynamically rendering of  side bar on home page only loged user can see side bar

---
+++ TaskManagement.Web/wwwroot/js/SideBar.js
@@ -0,0 +1,30 @@
+﻿// Write your JavaScript code.
+const sidebar = document.getElementById('sidebar');
+const dropdownElement = document.querySelector('.dropdown-menu');
+
+sidebar.addEventListener('mouseenter', () => {
+    sidebar.classList.remove('sidebar-collapsed');
+});
+
+sidebar.addEventListener('mouseleave', () => {
+    sidebar.classList.add('sidebar-collapsed');
+    dropdownElement.classList.remove('show');
+    dropdownElement.setAttribute('aria-expanded', 'false');
+});
+console.log('Sidebar hover functionality loaded', sidebar);
+// Active link highlighting
+const menuLinks = document.querySelectorAll('#sidebarMenu .nav-link');
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', function () {
+        menuLinks.forEach(l => l.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
+
+
+
+
+
+
+
