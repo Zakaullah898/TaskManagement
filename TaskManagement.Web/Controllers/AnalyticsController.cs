942eb4f9cbf397ff37de97e93043ba445c305318
Zakaullah898 <zakaullahlaar9@gmail.com>
2026-02-24 23:13:42 +0500

    Adding Analytics dashboard for all tasks detail and employee Analytics Dashboard where individual can see his tasks analytics

---
+++ TaskManagement.Web/Controllers/AnalyticsController.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TaskManagement.Application.DTOs;
+using TaskManagement.Domain.Entities;
+using TaskManagement.Domain.Interfaces;
+
+namespace TaskManagement.Web.Controllers
+{
+    public class AnalyticsController : Controller
+    {
+        private readonly IAuthService _authService;
+        private readonly ITaskService _taskService;
+        private readonly ITaskManagementRepo<TaskAssignments> _taskManagementRepo;
+        IMapper _mapper;
+        public AnalyticsController(
+            ITaskService taskService,
+            IMapper mapper,
+            IAuthService authService,
+            ITaskManagementRepo<TaskAssignments> taskManagementRepo)
+        {
+            _taskService = taskService;
+            _mapper = mapper;
+            _authService = authService; 
+            _taskManagementRepo = taskManagementRepo;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var tasks = await _taskService.GetAllTasks();
+            var taskDTOs = _mapper.Map<IEnumerable<TaskTableDTO>>(tasks);
+            foreach (var task in taskDTOs)
+            {
+
+                var assignedUser = await _taskManagementRepo.GetAsync(t => t.TaskId == task.TaskId, true);
+                if (assignedUser != null)
+                {
+                    //_logger.LogInformation("Task ID: {TaskId}, Title: {Title}, IsDeleted: {IsDeleted} , userId : {AssignedToUserId}", task.TaskId, task.Title, task.IsDeleted, assignedUser.AssignedToUserId);
+                    var user = await _authService.GetUserById(assignedUser.AssignedToUserId!);
+                    task.AssignedToUserName = user.UserName!;
+                }
+
+
+            }
+            return View(taskDTOs);
+        }
+        [HttpGet]
+        public async Task<IActionResult> EmployeeAnalyticAsync()
+        {
+            var tasks = await _taskService.GetAllTasks();
+            var taskDTOs = _mapper.Map<IEnumerable<TaskTableDTO>>(tasks);
+            foreach (var task in taskDTOs)
+            {
+
+                var assignedUser = await _taskManagementRepo.GetAsync(t => t.TaskId == task.TaskId, true);
+                if (assignedUser != null)
+                {
+                    //_logger.LogInformation("Task ID: {TaskId}, Title: {Title}, IsDeleted: {IsDeleted} , userId : {AssignedToUserId}", task.TaskId, task.Title, task.IsDeleted, assignedUser.AssignedToUserId);
+                    var user = await _authService.GetUserById(assignedUser.AssignedToUserId!);
+                    task.AssignedToUserName = user.UserName!;
+                }
+
+
+            }
+            return View(taskDTOs);
+        }
+    }
+}
--- TaskManagement.Web/Program.cs
+++ TaskManagement.Web/Program.cs
@@ -81,11 +81,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-//app.UseStaticFiles(new StaticFileOptions
-//{
-//    FileProvider = new PhysicalFileProvider(@"C:\Deploy\TaskManagementSystem\wwwroot\uploads"),
-//    RequestPath = "/uploads"
-//});
+// ?? IMPORTANT for Render (Linux)
+app.Urls.Add("http://0.0.0.0:10000");
+
 app.UseHttpsRedirection();
 app.UseRouting();
 // 3. Enable the session middleware
